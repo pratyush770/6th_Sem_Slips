0c715974906b65f948297e6f9faea89c848c2be4
pratyush majumdar <pratyushmajumdar27@gmail.com>
2024-02-11 23:02:50 +0530

    first commit

--- src/Practice/RunnableDemo.java
+++ src/Practice/RunnableDemo.java
@@ -9,18 +9,28 @@ public class RunnableDemo extends JFrame implements ActionListener,Runnable
     String s;
     char ch;
     Thread t=null;
+    Container c;
+    JPanel p1,p2;
     public RunnableDemo()
     {
         t1 = new JTextField(15);
         l1 = new JLabel("Enter a string to convert in uppercase : ");
         b1 = new JButton("Display");
         t = new Thread(this);
+        p1 = new JPanel();
+        p2 = new JPanel();
+        c = getContentPane();
         b1.addActionListener(this);
-        add(l1);add(t1);
-        add(b1);
+        p1.add(l1);p1.add(t1);
+        p2.add(b1);
+        p1.setLayout(new GridLayout(1,2));
+        p2.setLayout(new GridLayout(1,1));
+        c.add(p1);
+        c.add(p2);
         setVisible(true);
         setSize(500,500);
-        setLayout(new GridLayout(2,2));
+        setLayout(new GridLayout(2,1));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
     public void actionPerformed(ActionEvent e)
     {
