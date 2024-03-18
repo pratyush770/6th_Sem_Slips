5cb739dc54e9e3adf252155a61f0cfdeec134a4b
pratyush majumdar <pratyushmajumdar27@gmail.com>
2024-03-18 11:58:37 +0530

    first commit

--- src/Slip8A/slip8A.java
+++ src/Slip8A/slip8A.java
@@ -10,20 +10,20 @@ public class slip8A extends JFrame implements Runnable
         t = new Thread(this);
         l = new JLabel("");
         t.start();
+        setVisible(true);
+        setLayout(new FlowLayout());
+        setSize(400,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("SLIP8A");
+        setLocation(800,300);
+        l.setFont(new Font("Arial",Font.BOLD,28));
+        add(l);
     }
     @Override
     public void run()
     {
         try
         {
-            setVisible(true);
-            setLayout(new FlowLayout());
-            setSize(400,400);
-            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            setTitle("SLIP8A");
-            setLocation(800,300);
-            l.setFont(new Font("Arial",Font.BOLD,28));
-            add(l);
             while(true)
             {
                 l.setText("Welcome");
