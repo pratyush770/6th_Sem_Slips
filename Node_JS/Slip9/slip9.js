89ffa822c9150abe7d719ac5c630dd74ff0fa452
pratyush majumdar <pratyushmajumdar27@gmail.com>
2024-04-02 12:27:54 +0530

    first commit

--- Node_JS/Slip9/slip9.js
+++ Node_JS/Slip9/slip9.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const app = express();
-const multer = require('multer');  // used for uploading
-const port = 8080;
-app.get('/',(req,res)=>{
-    res.render("slip9.ejs");  // the ejs file must always be inside a folder named 'views'
+const multer = require('multer');
+const port = "8080";
+const path = require('path');
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.get("/",(req,res)=>{
+    res.render("slip9");
 });
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,'uploads/')  // uploads the files in the folder named 'uploads'
+        cb(null,'uploads/');
     },
     filename:function(req,file,cb){
-        cb(null,file.originalname)  // uploads the original file
+        cb(null,file.originalname);
     }
 });
 const upload = multer({storage:storage});
-app.post('/upload',upload.single('file'),(req,res)=>{ 
-     // upload.single('file') will return a single file
-    res.send("File upload successfully");
-})
+app.post('/upload',upload.single('file'),(req,res)=>{
+    res.send("File uploaded successfully");
+});
 app.listen(port,()=>{
     console.log(`Server is listening to port ${port}`);
 });
